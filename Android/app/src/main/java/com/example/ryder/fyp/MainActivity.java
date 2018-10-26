7871e8090c5d23c1cfd61cf91f5a665131e442a4
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-10-27 00:12:38 +0800

    Updated to include http utility methods for http get and post

--- Android/app/src/main/java/com/example/ryder/fyp/MainActivity.java
+++ Android/app/src/main/java/com/example/ryder/fyp/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.ryder.fyp.utility.HttpConnectionUtility;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -14,6 +16,8 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         EditText editText = (EditText) findViewById(R.id.editText3);
         //String message = ;
-        String my_url = "http://54.255.245.23:3000/user/retrieveAllUser";
+        String my_url = "http://54.255.245.23:3000/user/register";
         new MyHttpRequestTask().execute(my_url,"");
         Intent intent = new Intent(this, UserName.class);
         startActivity(intent);
@@ -37,62 +41,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-            String my_url = params[0];
-            String my_data = params[1];
-            try {
-                URL url = new URL(my_url);
-//                System.out.println(url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                // setting the  Request Method Type
-                httpURLConnection.setRequestMethod("GET");
-                // adding the headers for request
-                httpURLConnection.setRequestProperty("Content-Type", "application/text");
-                try{
-                    int responseCode = httpURLConnection.getResponseCode();
-                    System.out.println("Response Code: " + responseCode);
-                    if(responseCode == HttpURLConnection.HTTP_OK){
-                        BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-                        String input;
-                        StringBuffer response = new StringBuffer();
-
-                        while((input = in.readLine()) != null){
-                            response.append(input);
-                        }
-                        in.close();
-                        System.out.println(response.toString());
-                    }
-
-                    //to tell the connection object that we will be wrting some data on the server and then will fetch the output result
-//                    httpURLConnection.setDoOutput(true);
-//                    // this is used for just in case we don't know about the data size associated with our request
-//                    httpURLConnection.setChunkedStreamingMode(0);
-
-                    // to write tha data in our request
-//                    OutputStream outputStream = new BufferedOutputStream(httpURLConnection.getOutputStream());
-//                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-//                    outputStreamWriter.write(my_data);
-//                    outputStreamWriter.flush();
-//                    outputStreamWriter.close();
-
-                    // to log the response code of your request
-//                    Log.d(ApplicationConstant.TAG, "MyHttpRequestTask doInBackground : " +httpURLConnection.getResponseCode());
-//                     to log the response message from your server after you have tried the request.
-//                    //Log.d(ApplicationConstant.TAG, "MyHttpRequestTask doInBackground : " +httpURLConnection.getResponseMessage());
-
-
-                }catch (Exception e){
-                    e.printStackTrace();
-                }finally {
-                    // this is done so that there are no open connections left when this task is going to complete
-                    httpURLConnection.disconnect();
-                }
-
-
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-
-            return null;
+//            System.out.println("activated");
+            Map<String, String> req = new HashMap<>();
+//            req.put("username", "THINN");
+//            req.put("user_id", "10");
+//            req.put("team_id", "1");
+            String response = HttpConnectionUtility.post(params[0], req);
+            System.out.print("response: " + response);
+            return response;
         }
     }
 }
