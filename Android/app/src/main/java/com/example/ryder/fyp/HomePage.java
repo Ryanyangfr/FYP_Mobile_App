3c6fbf2dcae179010ce05060fbadee664abb3790
Kin Chia Heng <ryder.kin@hotmail.com>
2018-10-26 22:25:48 +0800

    Revert "Navigation"
    This reverts commit 48583835962f2be583410fcd9a4ff0ae45507818.

--- Android/app/src/main/java/com/example/ryder/fyp/HomePage.java
+++ Android/app/src/main/java/com/example/ryder/fyp/HomePage.java
@@ -1,11 +1,7 @@
 package com.example.ryder.fyp;
 
-import android.app.Fragment;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 public class HomePage extends AppCompatActivity {
 
@@ -13,34 +9,5 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
-
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    android.support.v4.app.Fragment selectedFragment = null;
-
-                    switch(item.getItemId()){
-                        case R.id.nav_home:
-                            selectedFragment = new HomeFragment();
-                            break;
-                        case R.id.nav_submissions:
-                            selectedFragment = new SubmissionsFragment();
-                            break;
-                        case R.id.nav_leaderboard:
-                            selectedFragment = new LeaderboardFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };
-
 }
--- Android/app/src/main/java/com/example/ryder/fyp/UserName.java
+++ Android/app/src/main/java/com/example/ryder/fyp/UserName.java
@@ -1,10 +1,7 @@
 package com.example.ryder.fyp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 
 public class UserName extends AppCompatActivity {
 
@@ -12,9 +9,5 @@ public class UserName extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_name);
-    }
-    public void sendUserName(View view) {
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
     }
 }
