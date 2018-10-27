b72c711f266dad320a48e9e410448eeff2993c71
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-10-27 21:57:41 +0800

    Updated to request permissions for external storage and sending of multipart form request

--- Android/app/src/main/java/com/example/ryder/fyp/MainActivity.java
+++ Android/app/src/main/java/com/example/ryder/fyp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.ryder.fyp;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ryder.fyp.utility.HttpConnectionUtility;
 
@@ -21,7 +29,7 @@ import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         EditText editText = (EditText) findViewById(R.id.editText3);
         //String message = ;
-        String my_url = "http://54.255.245.23:3000/user/register";
+        String my_url = "http://10.124.5.151:3000/upload/uploadSubmission";
         new MyHttpRequestTask().execute(my_url,"");
         Intent intent = new Intent(this, UserName.class);
         startActivity(intent);
@@ -42,13 +50,70 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 //            System.out.println("activated");
+            String filepath = "/storage/emulated/0/Pictures/ASCII.PNG";
             Map<String, String> req = new HashMap<>();
+//            String state = Environment.getExternalStorageState();
+            showPhoneStatePermission();
+//            if (Environment.MEDIA_MOUNTED.equals(state) ||
+//                    Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)){
+//                System.out.println("environment: " + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+//            }
 //            req.put("username", "THINN");
 //            req.put("user_id", "10");
 //            req.put("team_id", "1");
-            String response = HttpConnectionUtility.post(params[0], req);
+            String response = HttpConnectionUtility.multipartPost(params[0], new HashMap<String,String>(), filepath, "image", "image/png");
             System.out.print("response: " + response);
             return response;
         }
+    }
+
+    private void showPhoneStatePermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+                showExplanation("Permission Needed", "Rationale", Manifest.permission.READ_EXTERNAL_STORAGE, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
+            } else {
+                requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
+            }
+        } else {
+//            Toast.makeText(MainActivity.this, "Permission (already) Granted!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String permissions[],
+            int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+    private void showExplanation(String title,
+                                 String message,
+                                 final String permission,
+                                 final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermission(permission, permissionRequestCode);
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{permissionName}, permissionRequestCode);
     }
 }
