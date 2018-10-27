e40a76b6ee206882be277bb3b5d5ca3af2738c38
Kin Chia Heng <ryder.kin@hotmail.com>
2018-10-27 16:30:07 +0800

    implemented current location marker

--- Android/app/src/main/java/com/example/ryder/fyp/HomeFragment.java
+++ Android/app/src/main/java/com/example/ryder/fyp/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.ryder.fyp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -24,6 +26,8 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         mMapView = (MapView) v.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync((OnMapReadyCallback) this); //this is important
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
 
         return v;
     }
@@ -32,12 +36,18 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        try{
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+        catch (SecurityException e){
+            e.printStackTrace();
+        }
         LatLng Sob = new LatLng(1.2953, 103.8506);
-        LatLng Soa = new LatLng(1.2979, 103.8489);
-        LatLng SoeSoss = new LatLng(1.2974, 103.8495);
-        LatLng Sis = new LatLng(1.2949, 103.8495);
-        LatLng Sol = new LatLng(1.2962, 103.8501);
-        LatLng LksLib = new LatLng(1.2968, 103.8522);
+        LatLng Soa = new LatLng(1.2956, 103.8498);
+        LatLng SoeSoss = new LatLng(1.2979, 103.8489);
+        LatLng Sis = new LatLng(1.2974, 103.8495);
+        LatLng Sol = new LatLng(1.2949, 103.8495);
+        LatLng LksLib = new LatLng(1.2962, 103.8501);
         LatLng Admin = new LatLng(1.2968, 103.8522);
         mGoogleMap.addMarker(new MarkerOptions().position(Sob).title("School of Business"));
         mGoogleMap.addMarker(new MarkerOptions().position(Soa).title("School of Accountancy"));
@@ -47,6 +57,8 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         mGoogleMap.addMarker(new MarkerOptions().position(LksLib).title("Li Ka Shing Library"));
         mGoogleMap.addMarker(new MarkerOptions().position(Admin).title("SMU Admin Building"));
         mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Sis, 15));
+
+
     }
 
     @Override
