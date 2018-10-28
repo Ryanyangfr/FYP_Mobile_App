9aca681a7bb6a9954eaa54870040b7ffc316a16b
Nicholas Thian Chuan Hui <nicholasthian_1995@hotmail.com>
2018-10-28 15:13:50 +0800

    Added Camera Functions (CameraPage. file_paths.xml, edited submission fragment)

---
+++ Android/app/src/main/java/com/example/ryder/fyp/CameraPage.java
@@ -0,0 +1,133 @@
+package com.example.ryder.fyp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
+public class CameraPage extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Button button;
+    ImageView mImageView;
+    String mCurrentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_page);
+        button = findViewById(R.id.btnTakePicture);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                try {
+                    dispatchTakePictureIntent();
+                    galleryAddPic();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+                System.out.println(photoFile.getAbsolutePath());
+                System.out.println(photoFile.getName());
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+
+        }else{
+            Log.d("dispatchTakePictureIntent", "No applications with camera functionality");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            loadImagefromFile();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        System.out.println("Storage DIR" + storageDir.getAbsolutePath());
+        File image = File.createTempFile(
+                imageFileName,    /*prefix */
+                ".jpg",    /*suffix */
+                storageDir    /*directory*/
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    public void loadImagefromFile(){
+
+        mImageView = (ImageView)this.findViewById(R.id.imageView);
+        mImageView.setVisibility(View.VISIBLE);
+
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        //Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
+    }
+}
--- Android/app/src/main/java/com/example/ryder/fyp/SubmissionsFragment.java
+++ Android/app/src/main/java/com/example/ryder/fyp/SubmissionsFragment.java
@@ -1,5 +1,6 @@
 package com.example.ryder.fyp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +8,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SubmissionsFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_submissions,container, false);
+
+        final View rootView = inflater.inflate(R.layout.fragment_submissions,container, false);
+
+        final Button button = (Button) rootView.findViewById(R.id.btnToCamera);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent i = new Intent(rootView.getContext(), CameraPage.class);
+                startActivity(i);
+            }
+        });
+
+        return rootView;
     }
+
 }
